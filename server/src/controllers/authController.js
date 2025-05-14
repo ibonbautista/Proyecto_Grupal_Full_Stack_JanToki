6c04611c42b0e203ebd71bca3fa7d1bf2913de52
arakiztain <arakiztain.gaizka@gmail.com>
2025-05-14 13:45:31 +0200

    favorite ad review models/username register

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -20,13 +20,17 @@ const login = async(req,res)=>{
 }
 
 const register = async(req,res)=>{
-    const {email,password} = req.body;
+    const {username,email,password} = req.body;
+    const usernamaeUser = await userModel.findOne({username});
     const user = await userModel.findOne({email});
     if(user){
         return res.status(400).json({error:"Email already in use"});
     }
+    if(usernamaeUser){
+        return res.status(400).json({error:"Username already in use"});
+    }
     const hashedPassword = await bcrypt.hash(password,10);
-    const newUser = new userModel({email,password:hashedPassword});
+    const newUser = new userModel({email,password:hashedPassword},username);
     await newUser.save();
     res.json({message:"User created"});
 }
