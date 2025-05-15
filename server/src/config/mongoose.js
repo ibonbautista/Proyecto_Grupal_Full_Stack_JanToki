d23de66f0d038a316f6bd5663783b2cb7f43e75c
Sandra Fernandez <sandramedz@gmail.com>
2025-05-15 12:55:07 +0200

    prueba

--- server/src/config/mongoose.js
+++ server/src/config/mongoose.js
@@ -12,7 +12,7 @@ const DB_NAME = process.env.DB_NAME;
 export const connectDB = async () => {
     try {
         await mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`);
-        console.log("Connected to MongoDB");
+        console.log("HELLO, Connected to MongoDB");
     }catch (error) {
         console.error(error);
     }
