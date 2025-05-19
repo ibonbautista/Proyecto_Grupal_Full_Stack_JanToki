672362dd9114726b9467cb2e06bf91a9fb42c26e
ibonbautista <ibonbautista@gmail.com>
2025-05-19 21:54:42 +0200

    Instalacion de multer, cambios en rutas para modificar y eliminar imagenes subidas

--- server/src/routes/reviewRouter.js
+++ server/src/routes/reviewRouter.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import reviewController from '../controllers/reviewController.js';
+import { upload } from '../middlewares/multer.js';
 
 const router = express.Router();
 
 router.get('/user', reviewController.showReviewByUser);
 router.get('/restaurant/:restaurantId', reviewController.showReviewByRestaurant);
-router.post('/:restaurantId', reviewController.addReview);
+router.post('/:restaurantId', upload.single('image'),reviewController.addReview);
+router.put('/:reviewId/image', upload.single('image'), reviewController.updateReviewImage);
+router.delete('/:reviewId/image', reviewController.deleteReviewImage);
 router.put('/:id', reviewController.updateReview);
 router.delete('/:id', reviewController.deleteReview);
 
