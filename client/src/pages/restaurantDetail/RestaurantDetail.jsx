9860928fabb2504da81ca3c8aae115637bf1eca8
anais <acabado001@ehu.com>
2025-05-23 13:52:02 +0200

    estilo pagina restaurantDetail

---
+++ client/src/pages/restaurantDetail/RestaurantDetail.jsx
@@ -0,0 +1,108 @@
+import { useLoaderData, useNavigate } from "react-router-dom";
+import MapLeaflet from "../../components/mapLeaflet/MapLeaflet";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext, useEffect, useState } from "react";
+import { getReviewsByRestaurant } from "../../utils/api/review";
+import ReviewForm from "../../components/reviewForm/ReviewForm";
+import { useFavorite } from "../../hooks/useFavorite";
+
+import "./RestaurantDetail.css";
+
+function RestaurantDetail() {
+	const Navigate = useNavigate();
+
+	const { userData } = useContext(AuthContext);
+	const restaurant = useLoaderData();
+	const [reviews, setReviews] = useState([]);
+	const { isFavorite, toggleFavorite } = useFavorite(userData?.id, restaurant._id);
+
+	useEffect(() => {
+		const fetchReviews = async () => {
+			try {
+				const res = await getReviewsByRestaurant(restaurant._id);
+				setReviews(res.results || []);
+			} catch (error) {
+				if (error.message === "No reviews for this restaurant") {
+					setReviews([]);
+				} else {
+					console.error("Error al cargar reseñas:", error);
+				}
+			}
+		};
+		fetchReviews();
+	}, [restaurant._id]);
+
+	return (
+		<article className="restaurant-page">
+			<button className="back-button" onClick={() => Navigate(-1)}>
+				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
+					<path fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5" />
+				</svg>
+			</button>
+			<section className="restaurant-detail">
+				<div className="restaurant-page-image">
+					<h1>{restaurant.Name}</h1>
+					<img src={restaurant.Image} alt={restaurant.Name} className="restaurant-detail--image" />
+				</div>
+				<p className="restaurant-description">{restaurant.Description}</p>
+				<p className="cuisine-type">Tipo de cocina: {restaurant.Categories.CuisineType}</p>
+				<p className="restaurant-rating">Valoración del restaurante: {restaurant.rating}</p>
+				<div className="restaurant-address">
+					<MapLeaflet latitude={restaurant.Latitude} longitude={restaurant.Longitude} />
+					<p>{restaurant.Address}</p>
+					<p>{restaurant.Municipality}</p>
+					<p>{restaurant.Phone}</p>
+				</div>
+			</section>
+			<section className="restaurant-reviews">
+				<h2>Reseñas</h2>
+				{reviews.length === 0 ? (
+					<p>No hay reseñas todavía.</p>
+				) : (
+					<ul>
+						{reviews.map((review, i) => (
+							<li key={i}>
+								<strong>{review.user || "Anónimo"}:</strong> {review.text} - ⭐ {review.rating}
+								{review.image && (
+									<div>
+										<img
+											src={`http://localhost:3003/images/reviews/${review.image}`}
+											alt="Imagen de la reseña"
+										/>
+									</div>
+								)}
+							</li>
+						))}
+					</ul>
+				)}
+
+				{userData && (
+					<>
+						<div className="review-form">
+							<h3>Deja tu reseña</h3>
+							<ReviewForm
+								restaurantId={restaurant._id}
+								setReviews={setReviews}
+							/>
+						</div>
+						<div className="favorite-button">
+							<button onClick={toggleFavorite}>
+								{isFavorite ? (
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark-fill" viewBox="0 0 16 16" >
+										<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2z" />
+									</svg>) : (
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark" viewBox="0 0 16 16">
+										<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
+									</svg>)}
+							</button>
+						</div>
+					</>
+				)}
+
+				<p className="restaurant-website">{restaurant.Website}</p>
+			</section>
+		</article>
+	);
+}
+
+export default RestaurantDetail;
