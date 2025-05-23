4720ce0f47d338e413f6309c3fecb83ed7860321
Sandra Fernandez <sandramedz@gmail.com>
2025-05-23 10:29:14 +0200

    añadir a favoritos en restaurant detail

--- client/src/pages/restaurantDetail/restaurantDetail.jsx
+++ client/src/pages/restaurantDetail/restaurantDetail.jsx
@@ -4,11 +4,13 @@ import { AuthContext } from "../../context/AuthContext";
 import { useContext, useEffect, useState } from "react";
 import { getReviewsByRestaurant } from "../../utils/api/review";
 import ReviewForm from "../../components/reviewForm/ReviewForm";
+import { useFavorite } from "../../hooks/useFavorite";
 
 function RestaurantDetail() {
 	const { userData } = useContext(AuthContext);
 	const restaurant = useLoaderData();
 	const [reviews, setReviews] = useState([]);
+	const { isFavorite, toggleFavorite } = useFavorite(userData?.id, restaurant._id);
 	
 	useEffect(() => {
 		const fetchReviews = async () => {
@@ -66,11 +68,24 @@ function RestaurantDetail() {
 			
 			{userData && (
 				<>
+				<div className="review-form">
 					<h3>Deja tu reseña</h3>
 					<ReviewForm
 						restaurantId={restaurant._id}
 						setReviews={setReviews}
 					/>
+				</div>
+				<div className="favorite-button">
+					<button onClick={toggleFavorite}>
+					{isFavorite ? (
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark-fill" viewBox="0 0 16 16" >
+							<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2z" />
+						</svg>) : (
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark" viewBox="0 0 16 16">
+							<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
+						</svg>)}
+					</button>
+				</div>
 				</>
 			)}
 			</section>
